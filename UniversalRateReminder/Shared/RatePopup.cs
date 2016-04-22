b6ea7bc5cb0c7093d7c8e36d710aed2582d8c730
Rodrigo Díaz <r2d2rigo@gmail.com>
2016-04-22 14:47:22 +0100

    Revert "Changed UWP review URI as suggested by @lorenzofar."
    This reverts commit 9ff92e0bc6e7785afcb8499e9ccd44584e85f9c8.

--- UniversalRateReminder/Shared/RatePopup.cs
+++ UniversalRateReminder/Shared/RatePopup.cs
@@ -126,7 +126,7 @@ namespace UniversalRateReminder
             Title = DefaultTitle;
             Content = DefaultContent;
             RateButtonText = DefaultRateButtonText;
-            CancelButtonText = defa;
+            CancelButtonText = DefaultCancelButtonText;
             LaunchLimit = DefaultLaunchLimitForReminder;
 
             if (!ApplicationData.Current.LocalSettings.Containers.ContainsKey(UniversalRateReminderContainerName))
@@ -167,7 +167,7 @@ namespace UniversalRateReminder
                     {
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 #if WINDOWS_UWP
-                        Launcher.LaunchUriAsync(new Uri("ms-windows-store://review/?ProductId=" + Package.Current.Id.FamilyName));
+                        Launcher.LaunchUriAsync(new Uri("ms-windows-store:REVIEW?PFN=" + Package.Current.Id.FamilyName));
 #else
                         bool runningOnPhone = true;
 
